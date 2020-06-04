43576fc642a267f490f02b3b50b3477050f23697
Sean <Seanboyd10@gmail.com>
2020-06-04 00:39:21 -0500

    Added count down functionality to timer

--- tabada.js
+++ tabada.js
@@ -1,6 +1,8 @@
+
+// Setup
 var button = document.getElementById("my-button");
 var title = document.getElementById("title");
-var time = 0;
+var timeleft = 5;
 var myInterval = -1;
 
 button.addEventListener("click", function(event){
@@ -8,11 +10,7 @@ button.addEventListener("click", function(event){
   // Start Timer
   if(myInterval == -1){
     button.innerHTML = "Pause";
-
-    myInterval = setInterval(function(){
-      time++;
-      title.innerHTML = time;
-    }, 1000);
+    myInterval = setInterval(countdownTimer, 1000);
   }
 
   else{
@@ -21,6 +19,31 @@ button.addEventListener("click", function(event){
     myInterval = -1
   }
 });
+
+function countdownTimer(){
+  // Stop timer at zero
+  if (timeleft==0){
+    title.innerHTML = "Done!";
+    clearInterval(myInterval);
+  }
+
+  else{
+    title.innerHTML = convertSeconds(timeleft);
+    timeleft--;
+  }
+}
+
+function convertSeconds(s){
+  // Calculate
+  let minutes = Math.floor(s/60);
+  let seconds = s%60;
+
+  // Format
+  let formattedminutes = ("0" + minutes).slice(-2);
+  let formattedseconds = ("0" + seconds).slice(-2);
+  return formattedminutes + ':' + formattedseconds;
+}
+
 
 
 
